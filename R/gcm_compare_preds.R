5b37f86919448c6e9ea2d916e20aeae803e21eb7
sp-conway <58159926+sp-conway@users.noreply.github.com>
2020-08-11 11:17:05 -0500

    Update gcm_compare_preds.R

--- R/gcm_compare_preds.R
+++ R/gcm_compare_preds.R
@@ -2,7 +2,7 @@
 # Returns predicted data, sse
 gcm_compare_preds<-function(params, stim, categories,stim_names,exemplar_names,data){
   source('./gcm_pred.R')
-  pred_dat<-gcm_pred(params, stim, categories,stim_names,exemplar_names)[[1]]
+  pred_dat<-gcm_pred(params, stim, categories,stim_names,exemplar_names)
   sse<-sum((pred_dat$prob - data$prob)^2)
   return(sse)#list(pred_dat,sse))
 }
