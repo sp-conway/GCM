3f718c8990a8f9337c81c4d9273b6d433febcb5a
sp-conway <58159926+sp-conway@users.noreply.github.com>
2020-08-11 11:17:09 -0500

    Update gcm_samp_fit.R

--- R/sample_runs/gcm_samp_fit.R
+++ R/sample_runs/gcm_samp_fit.R
@@ -1,6 +1,4 @@
 # Getting the 'actual' data
-wd<-getwd()
-setwd(wd)
 source('./sample_runs/gen_data.R')
 
 # Function for calling gcm_pred, getting predictions, and comparing to data 
@@ -10,22 +8,22 @@ source('./gcm_compare_preds.R')
 library(optimx)
 
 # Making up starting values for parameters
-start_params<-c(10,.9,.1)
+start_params<-c(10,.01,.99)
 
 gcm_compare_preds(params=start_params,
                   stim=all_test_coords,
                   categories=exemplar_coords,
                   stim_names=stim_names,
                   exemplar_names = exemplar_cats,
-                  data=gen_dat)
+                  data=gen_data)
 
 fit<-optimx(par=start_params,
             fn=gcm_compare_preds,
             method = 'L-BFGS-B',
-            lower = c(0, 0,0),
+            lower = c(0,0,0),
             upper = c(10, 1,1),
             stim=all_test_coords,
             categories=exemplar_coords,
             stim_names=stim_names,
             exemplar_names = exemplar_cats,
-            data=gen_dat)
+            data=gen_data)
