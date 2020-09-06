fff86b05c154c33523590a65237df0c68a097419
sp-conway <58159926+sp-conway@users.noreply.github.com>
2020-09-05 23:05:23 -0500

    Still need to fix distance calculation

--- R/dist.R
+++ R/dist.R
@@ -1,45 +1,49 @@
 # Function for calculating distance between two matrices of stimuli per Nosofsky (1986)
 # ASSUMES DIMENSIONS ARE ORDERED IN THE SAME ORDER FOR EACH STIMULUS MATRIX
-# uses compmat function to compare all stimuli of one matrix to all of the other
+# uses compmats function to compare all stimuli of one matrix to all of the other
 # then calculates distance
 #
 #
 # Arguments
 # stim 1 - matrix of first set of stimuli
 # stim 2 - matrix of second set of stimuli
-# c - Nosofsky sensitivity parameter
-# w - Nosofsky attention weighting parameter
-# n_dims - # of dimensions stimuli vary along
+# c - Nosofsky sensitivity parameter - default is 1 
+# w - Nosofsky attention weighting parameter 
+# n_dims - # of dimensions stimuli vary along - default is ncol(stim1)
+# r - 1 = city-block distance, 2=Euclidean
 #
 # Returns
 # Matrix of distance between each stimulus of stim1 to each stimulus of stim2
 # Sean Conway
 # August 2020
-source('~/Box/RSean/comp_mat.R')
+source('~/Box/RSean/compmats.R')
 library(stringr)
-dist<-function(stim1, stim2,c,w,n_dims){
-  r<-2 # Euclidean distance. May need to change to 1 (city-block)
-  # using compmat function
-  # Because compmat will create column names in the form 'matrix_name'+'dimension_name',
-  # Can find which are the dimension names (and rule out observation names), by searching columns for matrix name
+dist<-function(stim1, stim2,c=1,w,n_dims,r){
   names<-c(deparse(substitute(stim1)),
            deparse(substitute(stim2)))
-  mat_1<-compmat(stim1,stim2)[[1]]
-  mat_2<-compmat(stim1,stim2)[[2]]
-  stim_numbers_1<-mat_1[,'Observation_Number_1']
-  stim_numbers_2<-mat_2[,'Observation_Number_2']
-  mat_1_dim_idx<-str_which(colnames(mat_1),names[[1]])
-  mat_2_dim_idx<-str_which(colnames(mat_2),names[[2]])
   
+  # using compmats function to get matrices ready to compare
+  mat_1<-compmats(stim1,stim2)[[1]]
+  mat_2<-compmats(stim1,stim2)[[2]]
+  
+  # get observation numbers, column names
+  mat_1_dim_idx<-str_which(colnames(mat_1),names[[1]])
+  stim_1_obs_num<-mat_1[,str_which(colnames(mat_1),names[[1]],negate = TRUE)]
+  mat_2_dim_idx<-str_which(colnames(mat_2),names[[2]])
+  stim_2_obs_num<-mat_2[,str_which(colnames(mat_2),names[[2]],negate = TRUE)]
+  
+  # matrices with just dimension values
   mat_1<-mat_1[,mat_1_dim_idx]
   mat_2<-mat_2[,mat_1_dim_idx]
   
+  # perform distance calculations
   dist_mat<-sapply(1:n_dims, function(i) mat_1[,i]-mat_2[,i])
   dist_mat<-sapply(1:n_dims, function(i) (dist_mat[,i]*w[i])^r)
   dist_mat<-cbind(dist_mat, 'dist'=rowSums(dist_mat))
   dist_mat[,'dist']<-c*(dist_mat[,'dist']^(1/r))
   
-  dist_mat<-cbind(dist_mat[,'dist'],stim_numbers_1, stim_numbers_2)
+  # get distance vals & observation numbers together
+  dist_mat<-cbind(dist_mat[,'dist'],stim_1_obs_num, stim_2_obs_num)
   colnames(dist_mat)<-c('dist',names)
   return(dist_mat)
 }
